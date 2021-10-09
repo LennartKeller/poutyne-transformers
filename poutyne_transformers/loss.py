916f5741869c092fe3632e933ff6adc0a1953aa3
Lennart Keller <lennart.keller@googlemail.com>
2021-10-09 11:41:35 +0200

    docs

--- poutyne_transformers/loss.py
+++ poutyne_transformers/loss.py
@@ -1,8 +1,17 @@
+import torch
 from typing import Any, Dict
 
 
-def model_loss(outputs: Dict[str, Any], targets: Any) -> float:
+def model_loss(outputs: Dict[str, Any], targets: Any) -> torch.tensor:
+    """Returns the internal loss of a transformers model.
+    Make sure that labels are passed into the transformers.
+
+    Args:
+        outputs (Dict[str, Any]): Model ouput of the transformer model
+        targets (Any): Pytounes labels, not used.
+
+    Returns:
+        torch.tensor: loss
     """
-    Returns the loss of the huggingface transformers model.
-    """
+
     return outputs["loss"]
