275ecea97b1d39d985917debd05a5de186218db2
Lennart Keller <lennart.keller@googlemail.com>
2021-10-12 12:25:38 +0200

    add save method && add type annotations

--- poutyne_transformers/model.py
+++ poutyne_transformers/model.py
@@ -1,13 +1,18 @@
+from typing import Any, Dict
 from torch import nn
+from transformers import PreTrainedModel
 
 
 class ModelWrapper(nn.Module):
-    def __init__(self, transformer):
+    def __init__(self, transformer: PreTrainedModel):
         super().__init__()
         self.transformer = transformer
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}({repr(self.transformer)})"
 
-    def forward(self, inputs):
+    def forward(self, inputs) -> Dict[str, Any]:
         return self.transformer(**inputs)
+
+    def save_pretrained(self, *args, **kwargs) -> None:
+        self.transformer.save_pretrained()
